4ec3b94fa16550db7116bf65f705723f151ddcd0
Benoit BRISSET <benoit.brisset@gmail.com>
2018-05-02 14:19:11 +0200

    Add const examples

--- tests/c++11/test_const.cpp
+++ tests/c++11/test_const.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <functional>
+#include <utility>
 
 using namespace std;
+
+const std::vector<int> & func_with_const_ref_return()
+{
+  std::vector<int> res = {1,2,3,4,5};
+  return res;
+}
+
+class sys{
+    protected:
+        vector<int> s;
+
+    public:
+        sys() {s = {1,2,3};}
+        const vector<int>& getS() {return s;}
+};
+
+
 
 /*
  * compile : g++ -std=c++11 test_const.cpp
@@ -36,6 +56,20 @@ int main()
                                        // A partir de ce moment la, le pointer n'est quand read only
                                        // et la data est en read only aussi.
     //(*p2) = 0; Ne marche pas
+  }
+
+  // const ref
+  {
+    // return a const ref
+    std::vector<int> res = func_with_const_ref_return();
+    // std::vector<int> & res_ref = func_with_const_ref_return();       // KO
+    // std::vector<int> & res_const_ref = func_with_const_ref_return(); // KO
+
+    sys* my_sys = new sys();
+    const vector<int> &t1 = my_sys->getS();
+    const vector<int> &t2 = my_sys->getS();
+    vector<int> t3 = my_sys->getS();
+     vector<int> t4 =  std::move(my_sys->getS());
   }
 
   return 0;
