21ef6a0c2e0554c56070cafef3cecb3a8483f250
Benoit Brisset <benoit.brisset@gmail.com>
2018-09-26 15:51:13 +0200

    Add backtring for sudoku step 1

--- tests/sudoku/sudoku.cc
+++ tests/sudoku/sudoku.cc
@@ -2,7 +2,9 @@
 #include <array>
 #include <iostream>
 #include <set>
+#include <stack>
 #include <random>
+#include <utility>
 
 using namespace std;
 
@@ -13,7 +15,6 @@ typedef array<uint, 9> Line;
 
 static array<uint, 9> base_line = {1,2,3,4,5,6,7,8,9};
 static const array<uint, 9> empty_line = {0,0,0,0,0,0,0,0,0};
-
 
 static const array<uint, 9> line_1 = {3,0,6,5,0,8,4,0,0};
 static const array<uint, 9> line_2 = {5,2,0,0,0,0,0,0,0};
@@ -32,36 +33,19 @@ public:
 
     Sudoku()
     {
-         _grid[0] = empty_line;
-         _grid[1] = empty_line;
-         _grid[2] = empty_line;
-         _grid[3] = empty_line;
-         _grid[4] = empty_line;
-         _grid[5] = empty_line;
-         _grid[6] = empty_line;
-         _grid[7] = empty_line;
-         _grid[8] = empty_line;
-
-         //_grid[0] = line_1;
-         //_grid[1] = line_2;
-         //_grid[2] = line_3;
-         //_grid[3] = line_4;
-         //_grid[4] = line_5;
-         //_grid[5] = line_6;
-         //_grid[6] = line_7;
-         //_grid[7] = line_8;
-         //_grid[8] = line_9;
+        clear();
     }
 
-    void generate() {
+    void generate()
+    {
 
         // fill diagonal
-        shuffle_line(base_line);
-        fill_sub_matrix(base_line, 0, 0);
-        shuffle_line(base_line);
-        fill_sub_matrix(base_line, 3, 3);
-        shuffle_line(base_line);
-        fill_sub_matrix(base_line, 6, 6);
+        //shuffle_line(base_line);
+        //fill_sub_matrix(base_line, 0, 0);
+        //shuffle_line(base_line);
+        //fill_sub_matrix(base_line, 3, 3);
+        //shuffle_line(base_line);
+        //fill_sub_matrix(base_line, 6, 6);
 
         // fill sub matrix
         fill_sub_matrix();
@@ -69,7 +53,8 @@ public:
 
     }
 
-    void print() {
+    void print()
+    {
         for (const auto& lines: _grid) {
             for (const auto& cell: lines) {
                 cout << cell;
@@ -78,8 +63,66 @@ public:
         }
     }
 
-    void backtracking() {
+    void clear()
+    {
+         //_grid[0] = empty_line;
+         //_grid[1] = empty_line;
+         //_grid[2] = empty_line;
+         //_grid[3] = empty_line;
+         //_grid[4] = empty_line;
+         //_grid[5] = empty_line;
+         //_grid[6] = empty_line;
+         //_grid[7] = empty_line;
+         //_grid[8] = empty_line;
 
+         _grid[0] = line_1;
+         _grid[1] = line_2;
+         _grid[2] = line_3;
+         _grid[3] = line_4;
+         _grid[4] = line_5;
+         _grid[5] = line_6;
+         _grid[6] = line_7;
+         _grid[7] = line_8;
+         _grid[8] = line_9;
+    }
+
+    pair<uint, uint> convert_in_coords(const uint pos)
+    {
+        return make_pair<uint,uint>(pos % 9, pos / 8);
+    }
+
+    void backtracking()
+    {
+        stack<pair<uint, uint>> s;
+        uint current_cell = 0;
+
+        // Add first cell
+        s.push(make_pair(0, _grid[0][0]));
+
+        while (!s.empty()) {
+
+            for (uint i = 0; i < 9; ++i) {
+
+                // If cell is empty
+                auto cell = s.top();
+                if (cell.second != 0) {
+
+                    auto coords = convert_in_coords(cell.first);
+                    if (check_neighbouring(i, coords.first, coords.second)) {
+
+                        _grid[coords.second][coords.first] = i;
+
+                        auto next_cell = convert_in_coords(cell.first + 1);
+                        s.push(make_pair(cell.first + 1, _grid[next_cell.first][next_cell.second]));
+                    } else {
+                        s.pop();
+                    }
+                } else {
+                    auto next_cell = convert_in_coords(cell.first + 1);
+                    s.push(make_pair(cell.first + 1, _grid[next_cell.first][next_cell.second]));
+                }
+            }
+        }
     }
 
 private:
@@ -87,20 +130,23 @@ private:
     Grid _grid;                       // my grid
     array< set<uint>, 9> _sub_matrix; // Use for sub matrix neighbouring
 
-    uint32_t random_number(const uint32_t min, const uint32_t max) {
+    uint32_t random_number(const uint32_t min, const uint32_t max)
+    {
         std::mt19937 rng;
         rng.seed(std::random_device()());
         std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
         return dist(rng);
     }
 
-    void shuffle_line(Line& line) {
+    void shuffle_line(Line& line)
+    {
         std::random_device rd;
         std::mt19937 g(rd());
         shuffle(line.begin(), line.end(), g);
     }
 
-    uint sub_matrix_idx(const uint x, const uint y) {
+    uint sub_matrix_idx(const uint x, const uint y)
+    {
         uint sub_x = x / 3;
         uint sub_y = y / 3;
 
@@ -108,8 +154,8 @@ private:
         return sub_x + sub_y*3;
     }
 
-    void fill_sub_matrix(Line& line, uint offset_x, uint offset_y) {
-
+    void fill_sub_matrix(Line& line, uint offset_x, uint offset_y)
+    {
         uint current_line = 0;
         uint current_cell = 0;
         for (uint i = 0; i < line.size(); ++i) {
@@ -122,12 +168,14 @@ private:
         }
     }
 
-    void fill_sub_matrix() {
+    void fill_sub_matrix()
+    {
+        uint current_x = 0;
+        uint current_y = 0;
+        uint nb_cell   = 0;
+        set<uint> values_tried;
 
-        uint current_x   = 0;
-        uint current_y   = 0;
-
-        for (int i = 0; i < 9*9 + 1; ++i) {
+        while (nb_cell < (9*9 + 1)) {
 
             uint new_val = random_number(1, 9);
 
@@ -136,10 +184,9 @@ private:
                 while(1) {
 
                     auto sm_idx = sub_matrix_idx(current_x, current_y);
-                    if (check_neighbouring(new_val, current_x, current_y, _sub_matrix[sm_idx])) {
+                    if (check_neighbouring(new_val, current_x, current_y)) {
 
                         _grid[current_y][current_x] = new_val;
-                        _sub_matrix[sm_idx].insert(new_val);
 
                         // next cell
                         current_x++;
@@ -147,14 +194,24 @@ private:
                             current_x = 0;
                             current_y++;
                         }
+                        values_tried.clear();
                         break;
 
                     } else {
+                        values_tried.insert(new_val);
+                        if (values_tried.size() == 9) {
+                            values_tried.clear();
+                            clear();
+                            nb_cell = 0;
+                            break;
+                        }
                         new_val++;
-                        if (new_val % 10 == 0) {
+                    if (new_val % 10 == 0) {
                             new_val = 1;
                         }
                     }
+                    print();
+                    cout << "\n";
                 }
             } else {
 
@@ -165,10 +222,14 @@ private:
                     current_y++;
                 }
             }
+            nb_cell++;
         }
+        print();
+        cout << "\n";
     }
 
-    bool check_neighbouring(uint number, uint offset_x, uint offset_y, set<uint> sub_neighbour) {
+    bool check_neighbouring(uint number, uint offset_x, uint offset_y)
+    {
         for (uint i = 0; i < _grid[offset_y].size(); ++i) {
             if (_grid[offset_y][i] == number) {
                 return false;
@@ -179,9 +240,13 @@ private:
                 return false;
             }
         }
-        for (const auto& set_val : sub_neighbour) {
-            if (set_val == number) {
-                return false;
+        uint row = offset_y - offset_y%3;
+        uint col = offset_x - offset_x%3;
+        for (row = 0; row < 3; ++row) {
+            for (col = 0; col < 3; ++col) {
+                if (_grid[row][col] == number) {
+                    return false;
+                }
             }
         }
         return true;
