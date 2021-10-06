48d00abeaad8cc10465502b1cfbf9b63777475b5
Benoit Brisset <benoit.brisset@gmail.com>
2021-10-06 12:43:40 +0200

    coin change

--- tests/dp/coin_change.cc
+++ tests/dp/coin_change.cc
@@ -4,10 +4,8 @@
 using namespace std;
 
 // Recursive
-// O(2*m*n)
-// Returns the count of ways we can
-// sum S[0...m-1] coins to get sum n
-int count(vector<int> S, int m, int total)
+// exponential time !!!!!
+int rec_count(vector<int> S, int m, int total)
 {
 
     // If n is 0 then there is 1 solution
@@ -28,63 +26,71 @@ int count(vector<int> S, int m, int total)
 
     // count is sum of solutions (i)
     // including S[m-1] (ii) excluding S[m-1]
-    return count(S, m - 1, total) +
-           count(S, m, total - S[m - 1]);
+    return rec_count(S, m - 1, total) +
+           rec_count(S, m, total - S[m - 1]);
+}
+
+int count(vector<int> const &S, int n, int target)
+{
+    // if the total is 0, return 1 (solution found)
+    if (target == 0) {
+        return 1;
+    }
+
+    // return 0 (solution does not exist) if total becomes negative,
+    // no elements are left
+    if (target < 0 || n < 0) {
+        return 0;
+    }
+
+    // Case 1. Include current coin `S[n]` in solution and recur
+    // with remaining change `target-S[n]` with the same number of coins
+    int include = count(S, n, target - S[n]);
+
+    // Case 2. Exclude current coin `S[n]` from solution and recur
+    // for remaining coins `n-1`
+    int exclude = count(S, n - 1, target);
+
+    // return total ways by including or excluding current coin
+    return include + exclude;
 }
 
 // O(m*n)
 int dp_count( vector<int> S, int m, int n )
 {
-    int i, j, x, y;
-
-    // We need n+1 rows as the table
-    // is constructed in bottom up
-    // manner using the base case 0
-    // value case (n = 0)
-    int table[n + 1][m];
-    for (int u = 0; u < n + 1; ++u) {
-        for (int w = 0; w < m; ++w)
-            table[u][w] = 0;
-    }
-
-    // Fill the entries for 0
-    // value case (n = 0)
-    for (i = 0; i < m; i++)
-        table[0][i] = 1;
-
-    // Fill rest of the table entries
-    // in bottom up manner
-    for (i = 1; i < n + 1; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            // Count of solutions including S[j]
-            x = (i-S[j] >= 0) ? table[i - S[j]][j] : 0;
-
-            // Count of solutions excluding S[j]
-            y = (j >= 1) ? table[i][j - 1] : 0;
-
-            // total count
-            table[i][j] = x + y;
+     // table[i] will be storing the number of solutions for
+     // value i. We need n+1 rows as the table is constructed
+     // in bottom up manner using the base case (n = 0)
+     int table[n+1];
+     // Initialize all table values as 0
+     memset(table, 0, sizeof(table));
+     // Base case (If given value is 0)
+     table[0] = 1;
+     // Pick all coins one by one and update the table[] values
+     // after the index greater than or equal to the value of the
+     // picked coin
+     for(int i = 0; i < m; i++){
+        for(int j = S[i]; j <= n; j++) {
+            table[j] += table[j-S[i]];
         }
-    }
-    return table[n][m - 1];
+     }
+     return table[n];
 }
 
-// Driver code
+
 int main()
 {
     vector<int> arr = { 1, 2, 3 };
     int m = arr.size();
-    cout << " " << count(arr, m, 4) << endl;
+    cout << " " << rec_count(arr, m, 4) << endl;
+    cout << " " << count(arr, m - 1, 4) << endl;
     cout << " " << dp_count(arr, m, 4) << endl;
 
     arr = { 1, 2 };
     m = arr.size();
-    cout << " " << count(arr, m, 20) << endl;
+    cout << " " << rec_count(arr, m, 20) << endl;
     cout << " " << dp_count(arr, m, 20) << endl;
 
     return 0;
 }
 
-// This code is contributed by shivanisinghss2110
