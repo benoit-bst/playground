9290ba0def16d1f2958ef4336da3208ad3e72b03
Benoit Brisset <benoit.brisset@gmail.com>
2022-01-14 09:47:28 +0100

    find_the_missing_number.cc with count

--- interview/array/find_the_missing_number.cc
+++ interview/array/find_the_missing_number.cc
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+// time : O(2*N)
+// space: O(N)
+int find_the_missing_number_counting(vector<int> arr)
+{
+    if (arr.size() == 0) {
+        return -1;
+    }
+
+    vector<int> counting(arr.size() + 1, 0);
+    for (int i = 0; i < arr.size(); i++) {
+        counting[arr[i] - 1]++;
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (counting[i] == 0) {
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
 // formula series :
 // Calculate the sum of first n natural numbers as sumtotal= n*(n+1)/2
 // time : O(N)
@@ -21,7 +42,15 @@ int find_the_missing_number(vector<int> arr)
 
 int main()
 {
-    vector<int> arr = { 3, 4, 9, 8, 7, 1, 2, 6};
+    vector<int> arr = { 1, 3, 5, 4};
+    cout << find_the_missing_number_counting(arr) << endl; // 2
+    cout << endl;
+
+    arr = { 3, 4, 9, 8, 7, 1, 2, 6};
+    cout << find_the_missing_number_counting(arr) << endl; // 5
+    cout << endl;
+
+    arr = { 3, 4, 9, 8, 7, 1, 2, 6};
     cout << find_the_missing_number(arr) << endl; // 5
 
     return 0;
